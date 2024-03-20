b5c55e6a99c94a5507ff94212834e32fc414e34a
Jeremy Khezzeri <jeremy.khezzeri@gmail.com>
2024-03-20 23:26:01 +0100

    add switch language button

--- main.js
+++ main.js
@@ -1,12 +1,52 @@
 
 const logo = document.querySelectorAll('.logo');
+const button_lang = document.querySelector('#buttonLang');
+const lang_list = document.querySelector('#langList');
 const button_mode = document.querySelector('#buttonMode');
 
-document.documentElement.setAttribute('data-theme', localStorage.getItem("mode"));
+document.documentElement.setAttribute('data-lang', localStorage.getItem("lang"));
+document.documentElement.setAttribute('data-mode', localStorage.getItem("mode"));
 
-if (document.documentElement.getAttribute('data-theme') == 'dark') {
-    button_mode.children[1].style.display = 'block';
+window.addEventListener('click', function(e){
+	if (button_lang.contains(e.target)){
+        lang_list.style.display = 'flex';
+    }
+    else {
+        lang_list.style.display = 'none';
+    }
+});
+
+const langs = Array.from(lang_list.children);
+
+langs.forEach(lang => {
+    lang.addEventListener('click',()=>{
+        langs.forEach(l => {
+            l.classList.remove('selected');
+        });
+        lang.classList.add('selected');
+        document.documentElement.setAttribute('data-lang', lang.innerHTML);
+        localStorage.setItem("lang", document.documentElement.getAttribute('data-lang'));
+    });
+});
+
+
+if (document.documentElement.getAttribute('data-lang') == 'fr') {
+
+    langs[0].classList.remove('selected');
+    langs[1].classList.add('selected');
+
+} else {
+
+    langs[0].classList.add('selected');
+    langs[1].classList.remove('selected');
+
+}
+
+
+
+if (document.documentElement.getAttribute('data-mode') == 'dark') {
     button_mode.children[0].style.display = 'none';
+    button_mode.children[1].style.display = 'block';
 
     logo.forEach(l => {
         l.style.filter="invert(1)";
@@ -21,8 +61,8 @@ if (document.documentElement.getAttribute('data-theme') == 'dark') {
 }
 
 button_mode.addEventListener('click',()=>{
-    if (document.documentElement.getAttribute('data-theme') == 'dark') {
-        document.documentElement.setAttribute('data-theme', 'light');
+    if (document.documentElement.getAttribute('data-mode') == 'dark') {
+        document.documentElement.setAttribute('data-mode', 'light');
         button_mode.children[0].style.display = 'block';
         button_mode.children[1].style.display = 'none';
 
@@ -31,7 +71,7 @@ button_mode.addEventListener('click',()=>{
         });
     }
     else {
-        document.documentElement.setAttribute('data-theme', 'dark');
+        document.documentElement.setAttribute('data-mode', 'dark');
         button_mode.children[1].style.display = 'block';
         button_mode.children[0].style.display = 'none';
 
@@ -39,7 +79,7 @@ button_mode.addEventListener('click',()=>{
             l.style.filter="invert(1)";
         });
     }
-    localStorage.setItem("mode", document.documentElement.getAttribute('data-theme'));
+    localStorage.setItem("mode", document.documentElement.getAttribute('data-mode'));
 });
 
 
