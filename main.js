d793e64a016395a97362c1c666a02ab2d5805806
Jeremy Khezzeri <jeremy.khezzeri@gmail.com>
2024-03-20 01:44:53 +0100

    add Bootsrap Icons + switch mode

--- main.js
+++ main.js
@@ -1,4 +1,51 @@
 
+const logo = document.querySelectorAll('.logo');
+const button_mode = document.querySelector('#buttonMode');
+
+document.documentElement.setAttribute('data-theme', localStorage.getItem("mode"));
+
+if (document.documentElement.getAttribute('data-theme') == 'dark') {
+    button_mode.children[1].style.display = 'block';
+    button_mode.children[0].style.display = 'none';
+
+    logo.forEach(l => {
+        l.style.filter="invert(1)";
+    });
+} else {
+    button_mode.children[0].style.display = 'block';
+    button_mode.children[1].style.display = 'none';
+
+    logo.forEach(l => {
+        l.style.filter="invert(0)";
+    });
+}
+
+button_mode.addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-theme') == 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        button_mode.children[0].style.display = 'block';
+        button_mode.children[1].style.display = 'none';
+
+        logo.forEach(l => {
+            l.style.filter="invert(0)";
+        });
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        button_mode.children[1].style.display = 'block';
+        button_mode.children[0].style.display = 'none';
+
+        logo.forEach(l => {
+            l.style.filter="invert(1)";
+        });
+    }
+    localStorage.setItem("mode", document.documentElement.getAttribute('data-theme'));
+});
+
+
+
+
+
 
 
 const current_year = document.querySelectorAll('.currentYear');
